70f93bcf0b54b8763d6f1d41eb6948042302f846
David Saunders <david@monkee-boy.com>
2017-12-05 12:21:37 -0600

    Set up React Router match and miss components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+// React Router allows you to show and hide components anywhere within your application depending on what page you're on
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 // Allows webpack to compile styles
 import './css/style.css';
-import App from './components/App';
 
-render(<App />, document.querySelector('#main') )
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+// The router itself is a component
+const Root = () => {
+  // Wrap entire router statement in BrowserRouter component
+  return (
+    <BrowserRouter>
+      {/* Match statements cannot be direct children of BrowserRouter. They need to be wrapped in a div */}
+      <div>
+        {/* When on homepage, show StorePicker component */}
+        <Match exactly pattern="/" component={StorePicker} />
+        {/* When on store page, show App component */}
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector('#main') )
+
