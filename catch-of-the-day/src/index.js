1312322a674b9508a8a2ccd34ca840e7411d567b
David Saunders <david@monkee-boy.com>
2017-12-05 11:52:33 -0600

    Created base files for Header, Order, and Inventory components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 // Allows webpack to compile styles
 import './css/style.css';
-import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker />, document.querySelector('#main') )
+render(<App />, document.querySelector('#main') )
