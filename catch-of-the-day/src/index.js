cf9eee7b247897d168fc6040e3b9f7af5efeb87d
David Saunders <david@monkee-boy.com>
2017-12-05 11:44:41 -0600

    Import styles

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+// Allows webpack to compile styles
+import './css/style.css';
 import StorePicker from './components/StorePicker';
 
 render(<StorePicker />, document.querySelector('#main') )
